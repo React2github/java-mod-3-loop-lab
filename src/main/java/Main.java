697e3fe84c5edb87b316796520aabb57d66c5a0c
Abdella <abdella.shukur2@gmail.com>
2022-06-22 13:50:33 -0400

    added new conditional logic to loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,14 @@ public class Main {
         // simulate the passing of time
         int startingYear = 2000;
         int targetYear = 2011;
-        int currentYear = startingYear;
+        int currentYear = startingYear + 1;
         while (currentYear < targetYear) {
+            if(currentYear == 2001) {
+                System.out.println((currentYear - startingYear) + " year has passed");
+            } 
+            else {
             System.out.println((currentYear - startingYear) + " year(s) have passed");
+            }
             // conditional logic based on the current year
             currentYear++;
         }
