33e36fbdb3c266f293a8a230b4ebfb5b4e1f699d
Abdella <abdella.shukur2@gmail.com>
2022-06-22 13:57:00 -0400

    added extension solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,15 @@ public class Main {
         // simulate the passing of time
         int startingYear = 2000;
         int targetYear = 2011;
-        int currentYear = startingYear + 1;
-        while (currentYear < targetYear) {
+        for(var i= startingYear; i < targetYear-1; i++) {
+            int currentYear = i + 1;
             if(currentYear == 2001) {
                 System.out.println((currentYear - startingYear) + " year has passed");
             } 
             else {
             System.out.println((currentYear - startingYear) + " year(s) have passed");
             }
-            // conditional logic based on the current year
-            currentYear++;
+
         }
     }
 }
